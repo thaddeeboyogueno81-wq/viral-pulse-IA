4f5b09378611054200dd7b70b66e810b690ff7d0
thaddeeboyogueno81-wq <thaddeeboyogueno81@gmail.com>
2026-09-03 12:44:42 +0100

    Change base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"viral-pulse-IA",
+  base:"/viral-pulse-IA/",
   server: {
     port: 5173,
     host: true
